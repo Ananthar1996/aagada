def66cff90d2322615e2e22f0adefb8cab91b162
Ananth <ananth.ar3@gmail.com>
2024-06-22 19:04:11 +0530

    SEO

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Head from "next/head";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -10,15 +10,47 @@ export const metadata = {
   icons: {
     icon: "/favicon-32x32.png",
   },
+  viewport: "width=device-width, initial-scale=1",
+  charset: "utf-8",
+  openGraph: {
+    title: "Agada Foods",
+    description:
+      "Online store for accessible and enjoyable shopping experience.",
+    url: "https://www.agadafoods.com",
+    siteName: "Agada Foods",
+    // images: [
+    //   {
+    //     url: "https://www.agadafoods.com/og-image.jpg",
+    //     width: 1200,
+    //     height: 630,
+    //   },
+    // ],
+    locale: "en_US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "WebSite",
+              name: "Agada Foods",
+              url: "https://www.agadafoods.com",
+            }),
+          }}
+        />
+      </body>
     </html>
   );
 }
