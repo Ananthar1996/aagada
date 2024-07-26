5445e68d80fdc0906c55e34478a69d1751e967ee
Ananth <ananth.ar3@gmail.com>
2024-07-26 08:06:36 +0530

    asd

--- components/Products.jsx
+++ components/Products.jsx
@@ -79,7 +79,7 @@ export default function Products() {
             <div className="w-[7rem] h-[3.5rem] sm:w-[10rem] sm:h-[6rem] bg-[#103535] rounded-tl-[2.5rem] rounded-br-[2.5rem] sm:rounded-tl-[3.4rem] sm:rounded-br-[3.4rem] flex items-center justify-center">
               <div className="text-[#F2F8EA] text-left px-2 sm:px-4 text-[10px] sm:text-lg">
                 <h3>URAD GOLA</h3>
-                <h6>MRP: 249</h6>
+                {/* <h6>MRP: 249</h6> */}
               </div>
             </div>
             <div className="w-[7rem] h-[2rem] sm:w-[10rem] sm:h-[4rem] bg-[#103535] rounded-tr-[2.9rem] rounded-bl-[2.9rem] sm:rounded-tr-[2.9rem] sm:rounded-bl-[3.2rem] flex items-center justify-center text-[#F2F8EA] text-left px-2 sm:px-4 text-[10px] sm:text-sm">
@@ -101,7 +101,7 @@ export default function Products() {
             <div className="w-[7rem] h-[3.5rem] sm:w-[10rem] sm:h-[6rem] bg-[#103535] rounded-tl-[2.5rem] rounded-br-[2.5rem] sm:rounded-tr-[3.4rem] sm:rounded-bl-[3.4rem] sm:rounded-tl-none sm:rounded-br-none flex items-center justify-center">
               <div className="text-[#F2F8EA] text-left px-2 sm:px-4 text-[10px] sm:text-lg">
                 <h3>URAD DAL</h3>
-                <h6>MRP: 259</h6>
+                {/* <h6>MRP: 259</h6> */}
               </div>
             </div>
             <div className="w-[7rem] h-[2rem] sm:w-[10rem] sm:h-[4rem] bg-[#103535] rounded-tr-[2.9rem] rounded-bl-[2.9rem] sm:rounded-tl-[2.9rem] sm:rounded-br-[3.2rem] sm:rounded-tr-none sm:rounded-bl-none flex items-center justify-center text-[#F2F8EA] text-left px-2 sm:px-4 text-[10px] sm:text-sm">
@@ -147,8 +147,11 @@ export default function Products() {
             <div className="w-[7rem] h-[3.5rem] sm:w-[10rem] sm:h-[6rem] bg-[#103535] rounded-tl-[2.5rem] rounded-br-[2.5rem] sm:rounded-tr-[3.4rem] sm:rounded-bl-[3.4rem] sm:rounded-tl-none sm:rounded-br-none flex items-center justify-center">
               <div className="text-[#F2F8EA] text-center px-2 sm:px-4 text-[8px] sm:text-sm">
                 <h3>BACK EYED BEANS</h3>
-                <h6>MRP: 179</h6>
+                {/* <h6>MRP: 179</h6> */}
               </div>
+            </div>
+            <div className="w-[7rem] h-[2rem] sm:w-[10rem] sm:h-[4rem] bg-[#103535] rounded-tr-[2.9rem] rounded-bl-[2.9rem] sm:rounded-tl-[2.9rem] sm:rounded-br-[3.2rem] sm:rounded-tr-none sm:rounded-bl-none flex items-center justify-center text-[#F2F8EA] text-left px-2 sm:px-4 text-[10px] sm:text-sm">
+              <h6>1Kg</h6>
             </div>
           </div>
         </div>
@@ -168,7 +171,7 @@ export default function Products() {
             <div className="w-[7rem] h-[3.5rem] sm:w-[10rem] sm:h-[6rem] bg-[#103535] rounded-tl-[2.5rem] rounded-br-[2.5rem] sm:rounded-tl-[3.4rem] sm:rounded-br-[3.4rem] flex items-center justify-center">
               <div className="text-[#F2F8EA] text-left px-2 sm:px-4 text-[10px] sm:text-[16px]">
                 <h3>GREEN MOONG</h3>
-                <h6>MRP: 239</h6>
+                {/* <h6>MRP: 239</h6> */}
               </div>
             </div>
             <div className="w-[7rem] h-[2rem] sm:w-[10rem] sm:h-[4rem] bg-[#103535] rounded-tr-[2.9rem] rounded-bl-[2.9rem] sm:rounded-tr-[2.9rem] sm:rounded-bl-[3.2rem] flex items-center justify-center text-[#F2F8EA] text-left px-2 sm:px-4 text-[10px] sm:text-sm">
@@ -190,7 +193,7 @@ export default function Products() {
             <div className="w-[7rem] h-[3.5rem] sm:w-[10rem] sm:h-[6rem] bg-[#103535] rounded-tl-[2.5rem] rounded-br-[2.5rem] sm:rounded-tr-[3.4rem] sm:rounded-bl-[3.4rem] sm:rounded-tl-none sm:rounded-br-none flex items-center justify-center">
               <div className="text-[#F2F8EA] text-center px-2 sm:px-4 text-[8px] sm:text-sm">
                 <h3>TAMARIND</h3>
-                <h6>MRP: 75</h6>
+                {/* <h6>MRP: 75</h6> */}
               </div>
             </div>
             <div className="w-[7rem] h-[2rem] sm:w-[10rem] sm:h-[4rem] bg-[#103535] rounded-tr-[2.9rem] rounded-bl-[2.9rem] sm:rounded-tl-[2.9rem] sm:rounded-br-[3.2rem] sm:rounded-tr-none sm:rounded-bl-none flex items-center justify-center text-[#F2F8EA] text-left px-2 sm:px-4 text-[10px] sm:text-sm">
